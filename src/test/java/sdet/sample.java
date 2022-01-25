72fa246513bc4ed4b8e4501475bce1db1dc87997
LENOVO <LENOVO@DESKTOP-R4ETFCJ>
2022-01-25 08:02:13 +0530

    1st automation 1st commit

--- src/test/java/sdet/sample.java
+++ src/test/java/sdet/sample.java
@@ -3,5 +3,6 @@ package sdet;
 public class sample {
 public static void main(String[] args) {
 	System.out.println("hi i m done with repositys");
+	System.out.println(" 1st autumation 1 st commit");
 }
 }
